76bc645df50a2bbd97bb5c3d936b4e3cd478241e
hello-ty <ty.gb3150.wwshz@gmail.com>
2021-05-26 23:48:32 +0900

    ⚗️ 不要な記述でいつか消しますが、クリックイベントを書いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,22 @@ import classes from 'src/styles/Home.module.css'
 import {Footer} from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import { Header } from 'src/components/Header'
+import { useCallback } from 'react'
+
+// const handleClick = (e) => {
+//   console.log(e.target.href)
+//   e.preventDefault();
+// };
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={classes.container}>
       <Head>
@@ -12,7 +26,10 @@ export default function Home() {
       </Head>
 
       <Header />
-
+      <a
+        href="/about" 
+        onClick={handleClick}
+      >ボタン</a>
       <Main page="index" />
       <Footer />
     </div>
