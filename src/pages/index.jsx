f292155e88c1dddd6b6390b64db7536224a7296f
hello-ty <ty.gb3150.wwshz@gmail.com>
2021-05-27 19:51:23 +0900

    ✨ useEffect説明用を導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import classes from 'src/styles/Home.module.css'
-import {Footer} from 'src/components/Footer'
-import { Main } from 'src/components/Main'
-import { Header } from 'src/components/Header'
-import { useCallback } from 'react'
+import Head from "next/head";
+import classes from "src/styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Main } from "src/components/Main";
+import { Header } from "src/components/Header";
+import { useCallback, useEffect } from "react";
 
 // const handleClick = (e) => {
 //   console.log(e.target.href)
@@ -12,6 +12,16 @@ import { useCallback } from 'react'
 
 export default function Home() {
   const foo = 1;
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
 
   const handleClick = useCallback((e) => {
     console.log(e.target.href);
@@ -26,12 +36,11 @@ export default function Home() {
       </Head>
 
       <Header />
-      <a
-        href="/about" 
-        onClick={handleClick}
-      >ボタン</a>
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
       <Footer />
     </div>
-  )
+  );
 }
